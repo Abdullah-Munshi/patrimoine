ed0eb5e9c954568cd31fb1b72630ba8982a7a2c0
Mrh Ripon <stackbind@gmail.com>
2023-07-04 11:12:46 +0600

    fixing commit

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const stickyTarget = document.querySelector(".sticky-target");
+  const stickyWrapper = document.querySelector("header");
+
+  const stickyWatch = (entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        stickyWrapper.classList.add("stuck");
+      } else {
+        stickyWrapper.classList.remove("stuck");
+      }
+    });
+  };
+  const observer = new IntersectionObserver(stickyWatch);
+  observer.observe(stickyTarget);
+
+  // Script for Mobile toggle button
+  if (document.querySelector(".navbar-toggle") !== null) {
+    const navbarToggle = document.querySelector(".navbar-toggle");
+    navbarToggle.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector("body").classList.toggle("extra");
+    });
+
+    $(".overlay").on("click", function () {
+      document.querySelector("body").classList.toggle("extra");
+    });
+  }
+
+  // Script for BV select activation
+  let language = new BVSelect({
+    selector: "#language",
+    width: "100%",
+  });
+});
